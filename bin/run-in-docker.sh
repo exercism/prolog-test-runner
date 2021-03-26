dd39f4c275b94891f5bcea4e0cab9ebe967a3ec2
Erik Schierboom <erik_schierboom@hotmail.com>
2021-03-26 14:14:38 +0100

    Add scripts to run test runner

---
+++ bin/run-in-docker.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+
+# Synopsis:
+# Run the test runner against a solution from the test runner Docker image
+# The test runner Docker image is built automatically
+
+# Arguments:
+# $1: exercise slug
+# $2: absolute path to solution folder
+# $3: absolute path to output directory
+
+# Output:
+# Writes the test results to a results.json file in the passed-in output directory.
+# The test results are formatted according to the specifications at https://github.com/exercism/docs/blob/main/building/tooling/test-runners/interface.md
+
+# Example:
+# ./bin/run-in-docker.sh two-fer /absolute/path/to/two-fer/solution/folder/ /absolute/path/to/output/directory/
+
+# if the required arguments are not provided, print usage and exit
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "usage: ./bin/run-in-docker.sh exercise-slug /absolute/path/to/solution/folder/ /absolute/path/to/output/directory/"
+    exit 1
+fi
+
+# build docker image
+docker build --rm -t prolog-test-runner .
+
+# create output directory if it doesn't exist
+output_dir="$3"
+mkdir -p "$output_dir"
+
+# run image passing the arguments
+docker run \
+    --network none \
+    --read-only \
+    --mount type=bind,src=$PWD/$2,dst=/solution \
+    --mount type=bind,src=$PWD/$output_dir,dst=/output \
+    --mount type=tmpfs,dst=/tmp \
+    prolog-test-runner $1 /solution /output
---
+++ bin/run.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+
+# Synopsis:
+# Run the test runner against a solution
+
+# Arguments:
+# $1: exercise slug
+# $2: absolute path to solution folder
+# $3: absolute path to output directory
+
+# Output:
+# Writes the test results to a results.json file in the passed-in output directory.
+# The test results are formatted according to the specifications at https://github.com/exercism/docs/blob/main/building/tooling/test-runners/interface.md
+
+# Example:
+# ./bin/run.sh two-fer /absolute/path/to/two-fer/solution/folder/ /absolute/path/to/output/directory/
+
+# if the required arguments are not provided, print usage and exit
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "usage: ./bin/run.sh exercise-slug /absolute/path/to/two-fer/solution/folder/ /absolute/path/to/output/directory/"
+    exit 1
+fi
+
+slug="$1"
+input_dir="${2%/}"
+output_dir="${3%/}"
+exercise="${slug//-/_}"
+implementation_file="${input_dir}/${exercise}.pl"
+tests_file="${input_dir}/${exercise}_tests.plt"
+results_file="${output_dir}/results.json"
+
+# create output directory if it doesn't exist
+mkdir -p "$output_dir"
+
+echo "$slug: testing..."
+
+test_output=$(swipl -f "$implementation_file" -s "$tests_file" -g run_tests,halt -t 'halt(1)' -- --all)
+
+# write results.json file based on the exit code of test run command that was just executed
+if [ $? -eq 0 ]; then
+    jq -n '{version: 1, status: "pass"}' > $results_file
+else
+    jq -n --arg message "${test_output}" '{version: 1, status: "fail", message: $message}' > $results_file
+fi
+
+echo "${slug}: done"
