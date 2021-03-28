1f43e8d78bcd0f297296035e12a1ba52afb82eac
Jeremy Walker <jez.walker@gmail.com>
2021-03-28 14:08:37 +0100

    Use output not message as the key

--- bin/run.sh
+++ bin/run.sh
@@ -43,7 +43,7 @@ test_output=$(swipl -f "${implementation_file}" -s "${tests_file}" -g run_tests,
 if [ $? -eq 0 ]; then
     jq -n '{version: 1, status: "pass"}' > ${results_file}
 else
-    jq -n --arg message "${test_output}" '{version: 1, status: "fail", message: $message}' > ${results_file}
+    jq -n --arg output "${test_output}" '{version: 1, status: "fail", output: $output}' > ${results_file}
 fi
 
 echo "${slug}: done"
